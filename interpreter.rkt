4521c7640a5973fd43fb8c3c028451e138554eaa
bison19 <rembrandtvanderploeg@gmail.com>
2020-02-15 14:15:38 -0500

    Added declare method in interpreter.rkt

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,9 +1,12 @@
 #lang racket
+(require "simpleParser.rkt")
+
 
 ; Implement  +,-,*,/,% as well as ==, !=,<,>,<=,>=, and &&,||,!.
 ; Variables may be true, false or an integer
 
 
+;We need to find a way to return a number if the expression is simply a number
 
 (define M_value
   (lambda (expression state)
@@ -16,3 +19,34 @@
       ((eq? '/ (car (cdr expression))) (quotient (M_value(car expression) '()) (M_value (car (cdr (cdr expression))) '() )))
       ((eq? '% (car (cdr expression))) (modulo (M_value(car expression) '()) (M_value (car (cdr (cdr expression))) '() )))
       )))
+
+(define declare
+  (lambda (input state)
+    (cond
+      ((null? state) (declare input '(()()) ))
+      ((null? input) state)
+      ((number? (car input)) (declare (cdr input) (cons (car state) (cons (list(car input)) (car(cdr state))))))
+      )))
+
+
+; Adds values and assoicated variables to the state list
+(define declare*
+  (lambda (vars vals state)
+    (cond
+      [(null? state) (declare* vars vals '(()()))] ;if the statelist is not implemented, sets up an empty statelist
+      [(null? vars) state] ;if we have added all vars, return statelist
+      ;if we are out of values, add the remaining vars to the statelist with a null value
+      [(null? vals) (declare* (cdr vars) vals (rebuild (cons (car vars) (car state)) (cons '() (cadr state))))]
+      ;else, add the var and it's associated values to the statelist.
+      [else (declare* (cdr vars) (cdr vals) (rebuild (cons (car vars) (car state)) (cons (car vals) (cadr state))))]
+      
+    )
+  )
+  )
+
+
+
+(define rebuild
+  (lambda (lis1 lis2)
+    (cons  lis1 (list lis2))
+    ))
