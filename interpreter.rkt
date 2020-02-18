57925a4d04e1815770d4c66d8e38223d1a723b9d
bison19 <rembrandtvanderploeg@gmail.com>
2020-02-17 20:56:18 -0500

    Added some condition checks, still need if, while, conditional, etc.

--- interpreter.rkt
+++ interpreter.rkt
@@ -6,6 +6,7 @@
 ; Implement  +,-,*,/,% as well as ==, !=,<,>,<=,>=, and &&,||,!.
 ; Variables may be true, false or an integer
 
+;if, if else, while, conditional
 
 ;We need to find a way to return a number if the expression is simply a number
 
@@ -14,25 +15,27 @@
     (cond
       ((null? expression) (error 'parser "parser should have caught this"))
       ((number? expression) expression)
-      ((eq? '+ (car expression)) (+ (M_value(car (cdr expression)) '()) (M_value (car (cdr (cdr expression))) '() )))
-      ((eq? '* (car expression)) (* (M_value(car (cdr expression)) '()) (M_value (car (cdr (cdr expression))) '() )))
-      ((eq? '- (car expression)) (- (M_value(car (cdr expression)) '()) (M_value (car (cdr (cdr expression))) '() )))
-      ((eq? '/ (car expression)) (quotient (M_value(car (cdr expression)) '()) (M_value (car (cdr (cdr expression))) '() )))
-      ((eq? '% (car expression)) (modulo (M_value(car (cdr expression)) '()) (M_value (car (cdr (cdr expression))) '() )))
+      ((eq? '+ (car expression)) (+ (M_value(cadr expression) '()) (M_value (caddr expression) '() )))
+      ((eq? '* (car expression)) (* (M_value(cadr expression) '()) (M_value (caddr expression) '() )))
+      ((eq? '- (car expression)) (- (M_value(cadr expression) '()) (M_value (caddr expression) '() )))
+      ((eq? '/ (car expression)) (quotient (M_value(cadr expression) '()) (M_value (caddr expression) '() )))
+      ((eq? '% (car expression)) (modulo (M_value(cadr expression) '()) (M_value (caddr expression) '() )))
       )))
 
-
-
-(define M_retrieve
-  (lambda (input state)
+(define M_boolean
+  (lambda (expression state)
     (cond
-      ((null? state) null)
-      ((null? input) null)
-      ((eq? input (caar state)) (caadr state))
-      (else (M_retrieve input (rebuild (cdr (car state)) (cdr (cadr state)))))
+      ((null? expression) (error 'parser "invalid op"))
+      ((eq? '== (car expression)) (eq? (M_value (cadr expression) '()) (M_value (caddr expression) '())))
+      ((eq? '!= (car expression)) (not (eq? (M_value (cadr expression) '()) (M_value (caddr expression) '()))))
+      ((eq? '< (car expression)) (< (M_value (cadr expression) '()) (M_value (caddr expression) '())))
+      ((eq? '> (car expression)) (> (M_value (cadr expression) '()) (M_value (caddr expression) '())))
+      ((eq? '<= (car expression)) (<= (M_value (cadr expression) '()) (M_value (caddr expression) '())))
+      ((eq? '>= (car expression)) (>= (M_value (cadr expression) '()) (M_value (caddr expression) '())))
+      ((eq? '&& (car expression)) (and (M_boolean (cadr expression) '()) (M_boolean (caddr expression) '())))
+      ((eq? '|| (car expression)) (or (M_boolean (cadr expression) '()) (M_boolean (caddr expression) '())))
+      ((eq? '! (car expression)) (not (M_boolean (cadr expression) '())))
       )))
-
-
 
 (define M_declare
   (lambda (var val state)
@@ -42,6 +45,37 @@
       [else (rebuild (cons var (car state)) (cons val (cadr state)))]
       )))
 
+(define return
+  (lambda (statement)
+    (cond
+    ((null? statement) #f)
+    ((and (eq? (car statement) 'return) (eq? (length statement) 2)) #t)
+    (else #f)
+    )))
+
+(define declaration
+  (lambda (statement)
+    (cond
+      ((null? statement) #f)
+      ((and (eq? (car statement) 'var) (eq? (length statement) 2)) #t)
+      (else #f)
+      )))
+
+(define declare-and-assign
+  (lambda (statement)
+    (cond
+      ((null? statement) #f)
+      ((and (eq? (car statement) 'var) (eq? (length statement) 3)) #t)
+      (else #f)
+      )))
+
+(define assignment
+  (lambda (statement)
+    (cond
+      ((null? statement) #f)
+      ((and (eq? (car statement) '=) (eq? (length statement) 2)) #t)
+      (else #f)
+      )))
 
 
 (define rebuild
